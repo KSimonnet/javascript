656dc52cb62eae368d150afc7e92b1d618ce8dfe
KSimonnet <k.simonnet@gmail.com>
2024-01-23 15:19:30 +1100

    Optimised filterTreeAgainstMask by by bringing arr_matches back into scope, creating traverseNode which left trim in the outter scope and simplifying match with every()

--- utility/manipJSON/filterTreeAgainstMask.js
+++ utility/manipJSON/filterTreeAgainstMask.js
@@ -7,58 +7,56 @@ import * as manipulateArray from "../manipArr/barrel-manipArr.js";
  * @param {boolean} trim - Condition as to whether to extract the full node or just the matched properties
  * @returns {Array} An array of matched nodes
  */
-const arr_matches = [];
+
 function filterTreeAgainstMask(obj_tree, mask_tree, trim) {
-  try {
-    // check if 'obj_tree' and 'mask_tree' are objects with enumerable string-keyed property names
-    if (
-      obj_tree &&
-      typeof obj_tree === "object" &&
-      mask_tree &&
-      typeof mask_tree === "object"
-    ) {
-      // in JavaScript, Arrays are of type Object. node.constructor === Array
-      if (Array.isArray(obj_tree)) {
-        // recursively traverse node for match with sub nodes
-        obj_tree.forEach((node) =>
-          filterTreeAgainstMask(node, mask_tree, trim),
-        );
-      } else {
-        const nodeKeys = Object.keys(obj_tree);
-        const maskKeys = Object.keys(mask_tree);
-        // check if all maskKeys match with this node's properties
-        const isMatch = maskKeys.every((key) => nodeKeys.includes(key));
-        if (isMatch) {
-          const match = maskKeys.filter((key) => {
-            const nodeValue = obj_tree[key];
-            const maskValue = mask_tree[key];
-            if (typeof nodeValue === "string") {
-              if (maskValue instanceof RegExp) {
-                return maskValue.test(nodeValue);
-              } else if (typeof maskValue === "string") {
-                return nodeValue === maskValue;
-              } else {
-                // TODO - break;
+  const arr_matches = [];
+  return traverseNode(obj_tree, mask_tree);
+  function traverseNode(obj_node, mask_node) {
+    try {
+      // check if 'obj_node' and 'mask_node' are objects with enumerable string-keyed property names
+      if (
+        obj_node &&
+        typeof obj_node === "object" &&
+        mask_node &&
+        typeof mask_node === "object"
+      ) {
+        // in JavaScript, Arrays are of type Object. node.constructor === Array
+        if (Array.isArray(obj_node)) {
+          // recursively traverse node for match with sub nodes
+          obj_node.forEach((node) => traverseNode(node, mask_node));
+        } else {
+          const nodeKeys = Object.keys(obj_node);
+          const maskKeys = Object.keys(mask_node);
+          // check if all maskKeys match with this node's properties
+          if (maskKeys.every((key) => nodeKeys.includes(key))) {
+            const match = maskKeys.filter((key) => {
+              const nodeValue = obj_node[key];
+              const maskValue = mask_node[key];
+              if (typeof nodeValue === "string") {
+                if (maskValue instanceof RegExp) {
+                  return maskValue.test(nodeValue);
+                } else if (typeof maskValue === "string") {
+                  return nodeValue === maskValue;
+                }
+              } else if (typeof nodeValue === typeof maskValue) {
+                traverseNode(nodeValue, maskValue);
               }
-            } else if (typeof nodeValue === typeof maskValue) {
-              filterTreeAgainstMask(nodeValue, maskValue, trim);
+            });
+            // check that all maskValues matched
+            if (match) {
+              arr_matches.push(trim ? _trim(obj_node, mask_node) : obj_node);
             }
-          });
-          // check that all maskValues matched
-          if (maskKeys.length === match.length) {
-            // arr_matches.push(trim ? _trim(node, mask_tree) : node);
-            arr_matches.push(trim ? _trim(obj_tree, mask_tree) : obj_tree);
           }
         }
       }
+      // return manipulateArray.convertArrToUtilityObj(arr_matches);
+      return arr_matches;
+    } catch (e) {
+      if (!(e instanceof Error)) {
+        let e = new Error(e);
+      }
+      console.error(e.message);
     }
-    // return manipulateArray.convertArrToUtilityObj(arr_matches);
-    return arr_matches;
-  } catch (e) {
-    if (!(e instanceof Error)) {
-      let e = new Error(e);
-    }
-    console.error(e.message);
   }
 }
 
