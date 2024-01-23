5f973fc2031499f42dd45ef739196f582932056d
KSimonnet <k.simonnet@gmail.com>
2024-01-23 11:21:54 +1100

    Modified the algo to include in the filter, not only the search criteria, but the mask structure

--- utility/manipJSON/filterTreeAgainstMask.js
+++ utility/manipJSON/filterTreeAgainstMask.js
@@ -1,66 +1,66 @@
 import * as manipulateArray from "../manipArr/barrel-manipArr.js";
 /**
- * Parses a tree object and filters it based on a mask of search criteria.
+ * Parses a tree object and filters it against a mask, based on its structure and search criteria.
  * inspired by: https://github.com/pichsenmeister/json-filter/tree/master
- * @param {Object} tree The object to search
- * @param {Object} mask The list of search criteria
- * @param {boolean} trim Whether to extract the full node or just the matched properties
+ * @param {Object} obj_tree - The object to search
+ * @param {Object} mask_tree - The list of search criteria
+ * @param {boolean} trim - Condition as to whether to extract the full node or just the matched properties
  * @returns {Array} An array of matched nodes
  */
-const filterTreeAgainstMask = (tree, mask, trim) => {
-  const arr_matches = [];
+const arr_matches = [];
+function filterTreeAgainstMask(obj_tree, mask_tree, trim) {
   try {
-    if (mask && typeof mask === "object") {
-      const maskKeys = Object.keys(mask);
-      const parseNode = (node) => {
-        // check tree is an object with enumerable string-keyed property names
-        if (node && typeof node === "object") {
-          const nodeKeys = Object.keys(node);
-          // in JavaScript, Arrays are of type Object. node.constructor === Array
-          if (Array.isArray(node)) {
-            node.forEach(parseNode);
-          } else {
-            // check if all maskKeys match with this node's properties
-            const isMatch = maskKeys.every((key) => nodeKeys.includes(key));
-            // parse paired values
-            if (isMatch) {
-              const match = maskKeys.filter((key) => {
-                const nodeValue = node[key];
-                const maskValue = mask[key];
-                if (
-                  typeof nodeValue === "string" &&
-                  maskValue instanceof RegExp
-                ) {
-                  return maskValue.test(nodeValue);
-                } else if (typeof maskValue !== "object") {
-                  return nodeValue === maskValue || maskValue === "$any";
-                } else if (typeof maskValue === "object") {
-                  // TODO handling maskValue as an object
-                }
-              });
-              // check if all maskKeys match
-              if (maskKeys.length === match.length) {
-                arr_matches.push(trim ? _trim(node, mask) : node);
+    // check if 'obj_tree' and 'mask_tree' are objects with enumerable string-keyed property names
+    if (
+      obj_tree &&
+      typeof obj_tree === "object" &&
+      mask_tree &&
+      typeof mask_tree === "object"
+    ) {
+      // in JavaScript, Arrays are of type Object. node.constructor === Array
+      if (Array.isArray(obj_tree)) {
+        // recursively traverse node for match with sub nodes
+        obj_tree.forEach((node) =>
+          filterTreeAgainstMask(node, mask_tree, trim),
+        );
+      } else {
+        const nodeKeys = Object.keys(obj_tree);
+        const maskKeys = Object.keys(mask_tree);
+        // check if all maskKeys match with this node's properties
+        const isMatch = maskKeys.every((key) => nodeKeys.includes(key));
+        if (isMatch) {
+          const match = maskKeys.filter((key) => {
+            const nodeValue = obj_tree[key];
+            const maskValue = mask_tree[key];
+            if (typeof nodeValue === "string") {
+              if (maskValue instanceof RegExp) {
+                return maskValue.test(nodeValue);
+              } else if (typeof maskValue === "string") {
+                return nodeValue === maskValue;
+              } else {
+                // TODO - break;
               }
+            } else if (typeof nodeValue === typeof maskValue) {
+              filterTreeAgainstMask(nodeValue, maskValue, trim);
             }
-            // recursively traverse node for match with sub nodes
-            nodeKeys.forEach((key) => {
-              parseNode(node[key]);
-            });
+          });
+          // check that all maskValues matched
+          if (maskKeys.length === match.length) {
+            // arr_matches.push(trim ? _trim(node, mask_tree) : node);
+            arr_matches.push(trim ? _trim(obj_tree, mask_tree) : obj_tree);
           }
         }
-      };
-
-      parseNode(tree);
-      return manipulateArray.convertArrToUtilityObj(arr_matches);
+      }
     }
+    // return manipulateArray.convertArrToUtilityObj(arr_matches);
+    return arr_matches;
   } catch (e) {
     if (!(e instanceof Error)) {
-      e = new Error(e);
+      let e = new Error(e);
     }
     console.error(e.message);
   }
-};
+}
 
 /**
  * Trims the tree object down to only the properties listed in the mask of search criteria.
