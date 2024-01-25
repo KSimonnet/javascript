00bfb6629cd46062ebfb74ef2c7d17a7c1b2a217
KSimonnet <k.simonnet@gmail.com>
2024-01-25 17:50:10 +1100

    Added Error handler. Created traverseNode function for internal recursion. Added default case to check  maskValue against nodeValue. Re-arranged return statement to handle source_node as an key-value object

--- utility/manipJSON/filterTreeAgainstMask.js
+++ utility/manipJSON/filterTreeAgainstMask.js
@@ -1,51 +1,51 @@
 /**
  * Parses a tree object and filters it against a mask, based on its structure and search criteria.
- * @param {Object} obj_tree - The object to search
- * @param {Object} mask_tree - The list of search criteria
- * @param {boolean} trim - Condition as to whether to extract the full node or just the matched properties
- * @returns {Array} An array of matched nodes
+ * @param {Object} source_node - The object to search
+ * @param {Object} mask_node - The search criteria tree
+ * @returns {Array} An array of matched root nodes
  */
-
 function filterTreeAgainstMask(source_node, mask_node) {
-  // check if 'source_node' and 'mask_node' are objects with enumerable string-keyed property names
-  if (
-    source_node &&
-    typeof source_node === "object" &&
-    mask_node &&
-    typeof mask_node === "object"
-  ) {
-    // in JavaScript, Arrays are of type Object. node.constructor === Array
+  if (typeof source_node !== "object" || typeof mask_node !== "object") {
+    throw new Error("Parameteris not an object");
+  }
+
+  try {
+    // in JavaScript, Arrays are of type Object
     if (Array.isArray(source_node)) {
       // recursively traverse sub nodes for match
       return source_node.filter((node) =>
         filterTreeAgainstMask(node, mask_node),
       );
     } else {
-      const nodeKeys = Object.keys(source_node);
-      const maskKeys = Object.keys(mask_node);
-      // check if all maskKeys match with this node's properties
-      if (!maskKeys.every((key) => nodeKeys.includes(key))) {
-        return false;
-      }
-      return maskKeys.every((key) => {
-        const nodeValue = source_node[key];
-        const maskValue = mask_node[key];
-        if (Array.isArray(nodeValue)) {
-          return nodeValue.some((node) =>
-            filterTreeAgainstMask(node, maskValue),
-          );
-        } else {
-          if (nodeValue.constructor.name === "String") {
-            if (maskValue instanceof RegExp) {
-              return maskValue.test(nodeValue);
-            } else {
-              return maskValue === nodeValue;
-            }
-          }
-        }
-      });
+      return traverseNode(source_node, mask_node) ? source_node : [];
     }
+  } catch (error) {
+    console.error(error);
   }
+}
+
+function traverseNode(node, mask) {
+  const maskKeys = Object.keys(mask);
+  // check if all maskKeys match with this node's properties
+  if (!maskKeys.every((key) => Object.keys(node).includes(key))) {
+    return false;
+  }
+  return maskKeys.every((key) => {
+    const nodeValue = node[key];
+    const maskValue = mask[key];
+
+    if (Array.isArray(nodeValue)) {
+      return nodeValue.some((node) => filterTreeAgainstMask(node, maskValue));
+    }
+    // nodeValue.constructor.name === "String"
+    if (typeof nodeValue === "string") {
+      return maskValue instanceof RegExp
+        ? maskValue.test(nodeValue)
+        : maskValue === nodeValue;
+    } else {
+      return maskValue === nodeValue;
+    }
+  });
 }
 
 export { filterTreeAgainstMask };
